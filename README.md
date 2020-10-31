04c767e1ef44a2896b443a7579a1f786c2fa2c04
tinfengyee <tinfengyee@163.com>
2020-10-31 14:06:49 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # vue-source-analysis
 
 
-
 [Vue.js 技术揭秘](https://github.com/ustbhuangyi/vue-analysis)
+
+
+# 速记
+
+join 和 resolve
---
+++ scripts/build.js
@@ -0,0 +1,10 @@
+console.log(process);
+
+console.log('+++++');
+
+console.log(process.env.NODE_ENV);
+
+
+console.log('--');
+
+console.log(process.argv);
